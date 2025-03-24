bd48935f6ee86f8847e9ff626424a8db5379a9ed
hyeoz <jenny970203@gmail.com>
2025-03-24 21:49:27 +0900

    add: 프라이버시 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,35 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import logo from './assets/logo_moving.gif'
+import Privacy from './pages/Privacy'
+
+function Home() {
+  return (
+    <div className='LinkList'>
+      <img src={logo} width={300} height={300} className='Logo' />
+      <ul>
+        <li className='LinkItem'>
+          <a href="https://apps.apple.com/kr/app/%EC%A7%81%EA%B4%80%EC%9D%BC%EA%B8%B0/id6503297796?l=en-GB" target="_blank">🍎 앱스토어</a>
+        </li>
+        <li className='LinkItem'>
+          <a href="https://play.google.com/store/apps/details?id=com.matchdiary.origin&hl=ko" target="_blank">▶️ 플레이 스토어</a>
+        </li>
+      </ul>
+      <p className='PrivacyLink'><a href="/privacy">개인정보 처리방침</a></p>
+    </div>
+  )
+}
 
 function App() {
-
   return (
-    <div className="Container">
-      <div className='LinkList'>
-        <img src={logo} width={300} height={300} className='Logo' />
-        <ul>
-          <li className='LinkItem'>
-            <a href="https://apps.apple.com/kr/app/%EC%A7%81%EA%B4%80%EC%9D%BC%EA%B8%B0/id6503297796?l=en-GB" target="_blank">🍎 앱스토어</a>
-          </li>
-          <li className='LinkItem'>
-            <a href="https://play.google.com/store/apps/details?id=com.matchdiary.origin&hl=ko" target="_blank">▶️ 플레이 스토어</a>
-          </li>
-        </ul>
+    <Router>
+      <div className="Container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/privacy" element={<Privacy />} />
+        </Routes>
       </div>
-    </div>
+    </Router>
   )
 }
 
